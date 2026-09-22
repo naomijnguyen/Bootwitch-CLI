d8fbd5f935f80a3ec5ea4e3b4b610b575eb0ac1e
Jennifer Naomi Nguyen <naomi@naomijnguyen.com>
2026-09-21 17:49:19 -0700

    chore(release): pin Homebrew formula to v0.4.0 archive

--- Formula/bootwitch.rb
+++ Formula/bootwitch.rb
@@ -1,9 +1,9 @@
 class Bootwitch < Formula
   desc "Portable project scaffolder for macOS and Linux"
   homepage "https://github.com/naomijnguyen/Bootwitch-CLI"
-  url "https://github.com/naomijnguyen/Bootwitch-CLI/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "4b14c7cad72afa3bb39f483c1c0e37b3cfd6a87d1234ef943990507a0893cf26"
-  version "0.3.0"
+  url "https://github.com/naomijnguyen/Bootwitch-CLI/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "ab6bb61cf08b33318e4ffdbcdbd34a12d2ed291beb3fcee46baf5480b79c5342"
+  version "0.4.0"
   license "MIT"
 
   depends_on "python@3.14"
